8696135da2015375e194fcf25aa54d547c8c47c2
e3brooks <53798202+e3brooks@users.noreply.github.com>
2024-07-08 16:57:41 -0700

    Associate models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    # associate comments
+    has_many :comments
+
     # handle invalid user input with validations
     validates :title, presence: true
     validates :body, presence: true, length: {minimum: 10}
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  # makes each comment belong to an article
   belongs_to :article
 end
