2fcd91d135b08320cfaa3491ef03bd85edf25f30
e3brooks <53798202+e3brooks@users.noreply.github.com>
2024-07-08 15:29:36 -0700

    Create new Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,25 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      # 422 Unprocessable Entity
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def article_params
+      # specify values allowed in param hash
+      params.require(:article).permit(:title, :body)
+    end
+
 end
