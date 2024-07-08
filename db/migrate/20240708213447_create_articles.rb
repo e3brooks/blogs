ea88486b183597fd87ff76d9363d47b6cde7d59f
e3brooks <53798202+e3brooks@users.noreply.github.com>
2024-07-08 14:42:26 -0700

    db migration

--- db/migrate/20240708213447_create_articles.rb
+++ db/migrate/20240708213447_create_articles.rb
@@ -1,9 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[7.1]
   def change
+    # create_table specifies how articles table should be constructed
     create_table :articles do |t|
+      # title and body generated because included in generate command
       t.string :title
       t.text :body
 
+      # defines 2 additional columns: created_at and updated_at
       t.timestamps
     end
   end
